4a613b7b13e9c592f0869e6ed3528236b7dbe910
Jean Camargo <jeancamargoreal@icloud.com>
2024-01-21 14:11:27 -0300

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Jean Camargo on 21/01/24.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
